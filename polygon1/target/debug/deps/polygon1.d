f4ac468b8491508d365a1bcba179d8bdc3cba16a
Isabella-22293 <116053798+Isabella-22293@users.noreply.github.com>
2024-07-23 22:55:56 -0600

    Estrella

---
+++ polygon1/target/debug/deps/polygon1.d
@@ -0,0 +1,9 @@
+D:\Documentos\Sexto semestre\Graficas\Graficas-Lab1\polygon1\target\debug\deps\polygon1.exe: src/main.rs src\framebuffer.rs src\line.rs src\polygon.rs src\bmp.rs
+
+D:\Documentos\Sexto semestre\Graficas\Graficas-Lab1\polygon1\target\debug\deps\polygon1.d: src/main.rs src\framebuffer.rs src\line.rs src\polygon.rs src\bmp.rs
+
+src/main.rs:
+src\framebuffer.rs:
+src\line.rs:
+src\polygon.rs:
+src\bmp.rs:
